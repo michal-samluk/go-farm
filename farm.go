8c449df06168c1550b592c3089492c91d0bcafa1
Michał Samluk <michal@codequest.com>
2015-10-31 00:38:03 +0100

    Farm API

--- farm.go
+++ farm.go
@@ -1,69 +1,20 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"os"
+	"github.com/gorilla/mux"
+	"github.com/michal-samluk/farm/animals"
+	"log"
+	"net/http"
 )
-
-type Species int
-
-const (
-	Cattle Species = iota
-	DomesticPig
-	WildHorse
-)
-
-var speciesNames = [3]string{
-	Cattle:      "Cattle",
-	DomesticPig: "Domestic pig",
-	WildHorse:   "Wild horse",
-}
-
-type Animal struct {
-	Species Species
-	Name    string
-	Age     int
-}
-
-func requestAnimal() *Animal {
-	animal := new(Animal)
-	for index, name := range speciesNames {
-		fmt.Printf("%d:%s\n", index, name)
-	}
-	fmt.Print("Select species: ")
-	fmt.Scanf("%d", &animal.Species)
-
-	fmt.Print("Name: ")
-	fmt.Scanf("%s", &animal.Name)
-
-	fmt.Print("Age: ")
-	fmt.Scanf("%d", &animal.Age)
-	return animal
-}
-
-func requestAnimals() (animals []*Animal) {
-	for answer := "Yes"; answer == "Yes"; {
-		animals = append(animals, requestAnimal())
-
-		fmt.Print("Type 'Yes' if you want to continue: ")
-		fmt.Scanf("%s", &answer)
-	}
-	return
-}
-
-func dumpAnimals(animals []*Animal) {
-	file, err := os.Create("farm.json")
-	defer file.Close()
-
-	if err != nil {
-		panic(err)
-	}
-
-	encoder := json.NewEncoder(file)
-	encoder.Encode(animals)
-}
 
 func main() {
-	dumpAnimals(requestAnimals())
+	r := mux.NewRouter()
+	r.HandleFunc("/animals/", animals.IndexHandler).Methods("GET")
+	r.HandleFunc("/animals/", animals.CreateHandler).Methods("POST")
+	http.Handle("/", r)
+
+	err := http.ListenAndServe(":3001", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
