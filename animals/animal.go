26be7b61cb4ed8b40c2e0ba028806527d658af68
Michał Samluk <michal@codequest.com>
2015-10-31 23:17:42 +0100

    Add custom handlers, save animals in DB

--- animals/animal.go
+++ animals/animal.go
@@ -3,12 +3,11 @@ package animals
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/michal-samluk/farm/app"
 	"net/http"
 )
 
 type Species int
-
-var savedAnimals []Animal
 
 const (
 	Pig Species = iota
@@ -16,35 +15,52 @@ const (
 	Cow
 )
 
-var SpeciesNames = [3]string{
-	Pig:   "Pig",
-	Horse: "Horse",
-	Cow:   "Cow",
-}
-
 type Animal struct {
+	ID      int
 	Species Species
 	Name    string
 	Age     int
 }
 
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+func IndexHandler(a *app.Context, w http.ResponseWriter, r *http.Request) (int, error) {
+	rows, err := a.DB.Query("SELECT id, name, age, species FROM animals")
+	if err != nil {
+		return http.StatusInternalServerError, err
+	}
+
+	var savedAnimals []Animal
+
+	defer rows.Close()
+	for rows.Next() {
+		var a = Animal{}
+		rows.Scan(&a.ID, &a.Name, &a.Age, &a.Species)
+		savedAnimals = append(savedAnimals, a)
+	}
 
 	encoder := json.NewEncoder(w)
 	encoder.Encode(savedAnimals)
+	return http.StatusOK, nil
 }
 
-func CreateHandler(w http.ResponseWriter, r *http.Request) {
+func CreateHandler(a *app.Context, w http.ResponseWriter, r *http.Request) (int, error) {
 	animal := new(Animal)
 
 	fmt.Sscanf(r.FormValue("species"), "%d", &animal.Species)
 	fmt.Sscanf(r.FormValue("name"), "%s", &animal.Name)
 	fmt.Sscanf(r.FormValue("age"), "%d", &animal.Age)
 
-	savedAnimals = append(savedAnimals, *animal)
+	err := a.DB.QueryRow(`
+    INSERT INTO animals(name, species, age)
+    VALUES($1, $2, $3) RETURNING id`,
+		animal.Name, animal.Species, animal.Age).Scan(&animal.ID)
 
-	w.Header().Set("Content-Type", "application/json")
+	if err != nil {
+		return http.StatusInternalServerError, err
+	}
+
 	encoder := json.NewEncoder(w)
-	encoder.Encode(animal)
+	if err = encoder.Encode(animal); err != nil {
+		return http.StatusInternalServerError, err
+	}
+	return http.StatusCreated, nil
 }
--- farm.go
+++ farm.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 	"github.com/michal-samluk/farm/animals"
+	"github.com/michal-samluk/farm/app"
 	"log"
 	"net/http"
 )
 
 func main() {
+	context := &app.Context{DB: nil}
+
+	connectDB(context)
+
 	r := mux.NewRouter()
-	r.HandleFunc("/animals/", animals.IndexHandler).Methods("GET")
-	r.HandleFunc("/animals/", animals.CreateHandler).Methods("POST")
+	r.Handle("/animals/", app.Handler{context, animals.IndexHandler}).Methods("GET")
+	r.Handle("/animals/", app.Handler{context, animals.CreateHandler}).Methods("POST")
 	http.Handle("/", r)
 
 	err := http.ListenAndServe(":3001", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func connectDB(c *app.Context) {
+	var err error
+	c.DB, err = sql.Open("postgres", "user=postgres dbname=farm_development sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
 	}
 }
